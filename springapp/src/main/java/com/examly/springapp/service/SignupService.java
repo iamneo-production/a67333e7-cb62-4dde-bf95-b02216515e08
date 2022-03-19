4f5442ab09efe8460f67e948d451acb31347ab7b
Ishan Gupta <gupta.ishan@hotmail.com>
2022-03-20 01:56:37 +0530

    Added Signup Controller

---
+++ springapp/src/main/java/com/examly/springapp/service/SignupService.java
@@ -0,0 +1,44 @@
+package com.examly.springapp.service;
+
+import com.examly.springapp.model.UserModel;
+import com.examly.springapp.repository.UserRepository;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class SignupService {
+    @Autowired
+    private UserRepository userRepository;
+    public UserModel saveUser(UserModel user){
+        return userRepository.save(user);
+    }
+    public List<UserModel> saveUsers(List<UserModel> user){
+        return userRepository.saveAll(user);
+    }
+    public List<UserModel> getUsers(){
+        return userRepository.findAll();
+    }
+    public UserModel getUsersById(UUID id){
+        return userRepository.findById(id).orElse(null);
+    }
+    public String deleteUser(UUID id){
+        userRepository.deleteById(id);
+        return "User removed of id"+id;
+    }
+    public UserModel updateUser(UserModel user){
+        UserModel existingUser= (UserModel) userRepository.findAllById(Collections.singleton(user.getId()));
+        existingUser.setEmail(user.getEmail());
+        existingUser.setUsername(user.getUsername());
+        existingUser.setPassword(user.getPassword());
+        existingUser.setMobileNumber(user.getMobileNumber());
+        existingUser.setRole(user.getRole());
+        return userRepository.save(existingUser);
+    }
+
+
+}
