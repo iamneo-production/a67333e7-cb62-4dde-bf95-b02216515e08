5110628bcf6a06b07c017c0ce7a31f0699ca2d4c
Ishan Gupta <gupta.ishan@hotmail.com>
2022-04-07 22:56:49 +0530

    Removed static url

--- reactapp/src/HomePages/ChatComponents/Chat.js
+++ reactapp/src/HomePages/ChatComponents/Chat.js
@@ -96,7 +96,7 @@ class ChatComponent extends Component {
               <iframe
                 width="560"
                 height="315"
-                src="https://www.youtube.com/embed/7c3-Gei5j4w"
+                src=""
                 title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
